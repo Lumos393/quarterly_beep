8c3aad81c483b10a0aa67e7a10035ea72a56f314
Lumos393 <64517124+Lumos393@users.noreply.github.com>
2020-09-07 00:49:54 -0400

    Update quarter_beep.py

--- quarter_beep.py
+++ quarter_beep.py
@@ -1,12 +1,20 @@
 from asyncio import subprocess
 import subprocess
 import time
-from time import sleep
 from playsound import playsound
 
-
-time_call = time.strftime('%H.%M')
-
 def quarter_beep():
-        playsound('D:\Documents Bulk\Sounds\windows_8_unlock.mp3')
-quarter_beep()
+    playsound('D:\Documents Bulk\Sounds\windows_8_unlock.mp3')
+
+hour_call = time.strftime('%H')
+minute_call = time.strftime('%M')
+
+if int(hour_call) in range(0,8):
+        if int(minute_call) == 00 or 15 or 30 or 45:
+                print(hour_call+':'+minute_call)
+                quarter_beep()
+                time.sleep(870)
+        else:
+                time.sleep(1)
+else:
+        time.sleep(50370)
