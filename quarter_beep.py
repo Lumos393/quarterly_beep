d29fe7ee681b1144fce1334eada2f0d2b563cef8
Lumos393 <64517124+Lumos393@users.noreply.github.com>
2020-09-08 02:28:56 -0400

    Update quarter_beep.py

--- quarter_beep.py
+++ quarter_beep.py
@@ -1,15 +1,14 @@
 import sys
 import os
-from pickle import FALSE
 import time
 import subprocess
+import numpy
+from numpy.ma.core import array
 from playsound import playsound
 from datetime import datetime
 import decouple
 
-mornran = range(0, 8)
-dayran = range(8, 19)
-nightran = range(19, 24)
+hourarr = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 minarr = [0, 15, 30, 45]
 
 hour_call = time.strftime('%H')
@@ -17,16 +16,12 @@ int_hour_call = int(time.strftime('%H'))
 minute_call = time.strftime('%M')
 int_minute_call = int(time.strftime('%M'))
 
-print(hour_call + ':' + minute_call)
-
-for x in dayran:
+while True:
         if int_minute_call in minarr:
                 print(hour_call, minute_call)
                 playsound('D:\Documents Bulk\Sounds\windows_8_unlock.mp3')
                 time.sleep(870)
-        if((int_hour_call in (range(0, 8) or range(19, 24))) and ((int_minute_call in minarr) == FALSE)):
+        if((int_hour_call in hourarr) and ((int_minute_call in minarr) == False)):
                 print('Waiting...')
-                time.sleep(1) or range(0, 8)
-        if int_hour_call in (mornran or nightran):
-                print('Nighty night')
-                time.sleep(1)
+        if (int_hour_call in hourarr) == False:
+                print('.')
