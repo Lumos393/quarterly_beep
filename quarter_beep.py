93596d8f144ec41d7c8ed25a7300ca9f1427daed
Lumos393 <64517124+Lumos393@users.noreply.github.com>
2020-09-08 02:57:48 -0400

    Update quarter_beep.py

--- quarter_beep.py
+++ quarter_beep.py
@@ -4,24 +4,19 @@ import time
 import subprocess
 import numpy
 from numpy.ma.core import array
-from playsound import playsound
-from datetime import datetime
-import decouple
+import winsound
 
 hourarr = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 minarr = [0, 15, 30, 45]
 
-hour_call = time.strftime('%H')
-int_hour_call = int(time.strftime('%H'))
-minute_call = time.strftime('%M')
-int_minute_call = int(time.strftime('%M'))
-
 while True:
-        if int_minute_call in minarr:
-                print(hour_call, minute_call)
-                playsound('D:\Documents Bulk\Sounds\windows_8_unlock.mp3')
-                time.sleep(870)
-        if((int_hour_call in hourarr) and ((int_minute_call in minarr) == False)):
-                print('Waiting...')
-        if (int_hour_call in hourarr) == False:
-                print('.')
+        int_hour_call = int(time.strftime('%H'))
+        int_minute_call = int(time.strftime('%M'))
+
+        if (int_hour_call in hourarr) and (int_minute_call in minarr):
+                winsound.PlaySound('D:\Documents Bulk\Sounds\windows_8_unlock.mp3')
+                time.sleep(60)
+        if (int_minute_call not in minarr):
+                time.sleep(5)
+        if (int_hour_call not in hourarr):
+                time.sleep()
